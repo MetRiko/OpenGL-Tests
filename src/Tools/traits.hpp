d465ad211169ea3552ecdff43ba650f777549123
MetRiko <18maximus8050@gmail.com>
2019-06-28 23:00:04 +0200

    * Trait to get template params of glm::vec

--- src/Tools/traits.hpp
+++ src/Tools/traits.hpp
@@ -14,6 +14,26 @@ namespace gl::traits {
 	};
 	template <class T> using remove_cvref_t = typename remove_cvref<T>::type;
 
+	// Read data (size, type, precision) from glm::vec 
+
+	template <class X>
+	struct read_glm_vec {
+
+		// Declaration
+		template <class T> struct Impl;
+
+		// Instance (partial specialization)
+		template <template <int, typename, glm::qualifier> class V, int S, typename T, glm::qualifier P>
+		struct Impl<V<S, T, P>> {
+		    static constexpr int size = S;
+		    using type = T;
+		    static constexpr glm::qualifier precision = P;
+		};
+
+		using data = Impl<remove_cvref_t<X>>;
+	};
+	template <class V> using read_glm_vec_d = typename read_glm_vec<V>::data;
+
 	// Check if <T> is glm::vec
 
 	template <class>
