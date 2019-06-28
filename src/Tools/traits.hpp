891e7d78fcbd05aa7222f8d975597e37af0daeb3
MetRiko <18maximus8050@gmail.com>
2019-06-28 22:59:02 +0200

    * Trait to check if type is glm::vec

--- src/Tools/traits.hpp
+++ src/Tools/traits.hpp
@@ -13,4 +13,15 @@ namespace gl::traits {
 		using type = std::remove_cv_t<std::remove_reference_t<T>>;
 	};
 	template <class T> using remove_cvref_t = typename remove_cvref<T>::type;
+
+	// Check if <T> is glm::vec
+
+	template <class>
+	struct is_glm_vec : std::false_type {};
+
+	template <int S, typename T, glm::qualifier P>
+	struct is_glm_vec<glm::vec<S,T,P>> : std::true_type {};
+
+	template <class X>
+	static constexpr bool is_glm_vec_v = is_glm_vec<X>::value;
 }
